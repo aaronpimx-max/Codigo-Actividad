1335ce2d48785787141d670362e8117684022fe2
aaronpimx-max <aaronpimx@gmail.com>
2026-04-27 17:35:22 -0300

    Add files via upload

---
+++ ConsoleApp47/ConsoleApp47/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using static ConsoleApp47.Program;
+
+namespace ConsoleApp47
+{
+    internal class Program
+    {
+        public class Cliente
+        {
+            private int _id;
+            private string _nombre;
+
+            public int Id
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+            public string Nombre
+            {
+                get { return _nombre; }
+                set { _nombre = value; }
+            }
+
+            public Decimal Credito { get; set; }
+
+            public void Comprar(decimal monto)
+            {
+                Credito += monto;
+            }
+
+            public void Vender(decimal monto)
+            {
+                Credito += monto;
+            }
+        }
+
+        
+            public static void RealizarTransacciones()
+            {
+                Cliente MiCliente = new Cliente();
+
+                MiCliente.Nombre = "Cliente Inicial";
+                MiCliente.Vender(100m);
+                Console.WriteLine($"Nombre: {MiCliente.Nombre}, Crédito actual: {MiCliente.Credito}");
+
+                MiCliente.Nombre = "Cliente Actualizado";
+                MiCliente.Vender(500m);
+                Console.WriteLine($"Nombre: {MiCliente.Nombre}, Crédito acumulado: {MiCliente.Credito}");
+            }
+        
+
+        static void Main(string[] args)
+            {
+             Console.WriteLine("Objetos y clases:");
+
+              RealizarTransacciones();
+
+            Console.ReadKey();
+            }  
+    }
+}
